dcd3ae829d3208606436f1c281b75962ff0bdbd8
phoenixjmh <phoenixjmh@gmail.com>
2023-03-10 18:56:00 -0700

    OK. the game logic works. The next step is probably cleaning up and actually seeing WHY things work. WHY DOESN'T GET TILE WORK SOMETIMES

--- script.js
+++ script.js
@@ -1,135 +1,183 @@
 /* eslint-disable guard-for-in */
 
-function Player(name,gamePiece,isTurn){
-    this.name = name;
-    
-    this.gamePiece=gamePiece;
+function Player(name, gamePiece, isTurn) {
+  this.name = name;
 
-    function getIsTurn()
-    {
-        return this.isTurn;
-    }
-    function setIsTurn()
-    {
-        this.isTurn =isTurn;
-        return this.isTurn;
-    }
-    function switchTurn()
-    {
-        this.isTurn=!this.isTurn;
-        return this.isTurn;
-    }
+  this.gamePiece = gamePiece;
 
-    return{name,getIsTurn,setIsTurn,switchTurn};
+  function getIsTurn() {
+    return this.isTurn;
+  }
+  function setIsTurn() {
+    this.isTurn = isTurn;
+    return this.isTurn;
+  }
+  function switchTurn() {
+    this.isTurn = !this.isTurn;
+    return this.isTurn;
+  }
+
+  return { name, getIsTurn, setIsTurn, switchTurn };
 }
-function Tile(id,contents,isBlank=true){
-    this.id = id;
-    this.contents=contents;
+function Tile(id, contents, isBlank = true) {
+  this.id = id;
+  this.contents = contents;
 
-    // eslint-disable-next-line no-return-assign
-    const setTile = newContents=> this.contents=newContents;
-    const getTile = ()=>this.contents;
-    
-   function setBlankFalse(){
-    isBlank=false;
-    this.isBlank=isBlank;
+  // eslint-disable-next-line no-return-assign
+  const setTile = (newContents) => (this.contents = newContents);
+   this.getTile = () => this.contents;
+
+  function setBlankFalse() {
+    isBlank = false;
+    this.isBlank = isBlank;
     return this.isBlank;
-   }
-    function getIsBlank()
-    {
-        this.isBlank=isBlank;
-        return this.isBlank;
-    }
+  }
+  function getIsBlank() {
+    this.isBlank = isBlank;
+    return this.isBlank;
+  }
 
-    return {setTile,getTile,id,contents,setBlankFalse,getIsBlank};
+  return { setTile, getTile, id, contents, setBlankFalse, getIsBlank };
 }
-const gameBoard =(function()
-{
-    const gameBoardContainer = document.querySelector('.game-board');
-    const tiles = [];
-    const tile1 = Tile('tile1','');
-    const tile2 = Tile('tile2','');
-    const tile3 = Tile('tile3','');
-    const tile4 = Tile('tile4','');
-    const tile5 = Tile('tile5','');
-    const tile6 = Tile('tile6','');
-    const tile7 = Tile('tile7','');
-    const tile8 = Tile('tile8','');
-    const tile9 = Tile("tile9", '');
-    tiles.push(tile1,tile2,tile3,tile4,tile5,tile6,tile7,tile8,tile9);
+const gameBoard = (function () {
+  const gameBoardContainer = document.querySelector(".game-board");
+  const tiles = [];
+  const tile1 = Tile("tile1", "");
+  const tile2 = Tile("tile2", "");
+  const tile3 = Tile("tile3", "");
+  const tile4 = Tile("tile4", "");
+  const tile5 = Tile("tile5", "");
+  const tile6 = Tile("tile6", "");
+  const tile7 = Tile("tile7", "");
+  const tile8 = Tile("tile8", "");
+  const tile9 = Tile("tile9", "");
+  tiles.push(tile1, tile2, tile3, tile4, tile5, tile6, tile7, tile8, tile9);
 
-    const render=()=>{
-        
-       
-        // eslint-disable-next-line no-restricted-syntax
-        for(const t in tiles)
-        {
-            
-            const tileContainer = document.createElement('div');
-            tileContainer.className='tile';
-            tileContainer.id = tiles[t].id;
-            const tileContents = document.createElement('div')
-            tileContents.className = 'contents';
-            tileContents.textContent = tiles[t].contents;
-            tileContainer.appendChild(tileContents);
-            gameBoardContainer.appendChild(tileContainer);
-        }
-    };
+  const render = () => {
+    // eslint-disable-next-line no-restricted-syntax
+    for (const t in tiles) {
+      const tileContainer = document.createElement("div");
+      tileContainer.className = "tile";
+      tileContainer.id = tiles[t].id;
+      const tileContents = document.createElement("div");
+      tileContents.className = "contents";
+      tileContents.textContent = tiles[t].contents;
+      tileContainer.appendChild(tileContents);
+      gameBoardContainer.appendChild(tileContainer);
+    }
+  };
+
+  const checkIfWin = (symbolToCheck) =>{
+    if(tiles[0].contents===symbolToCheck &&tiles[1].contents===symbolToCheck &&tiles[2].contents===symbolToCheck ) // TOP HORZ
+    {
+        gameLoop.endGame(symbolToCheck);
+        gameLoop.setGameOver();
+    }
+    if(tiles[3].contents===symbolToCheck &&tiles[4].contents===symbolToCheck &&tiles[5].contents===symbolToCheck ) // MIDDLE HORZ
+    {
+        gameLoop.endGame(symbolToCheck);
+    }
     
-    return{tiles,render,gameBoardContainer};
+    if(tiles[6].contents===symbolToCheck &&tiles[7].contents===symbolToCheck &&tiles[8].contents===symbolToCheck ) // BOTTOM HORZ
+    {
+        gameLoop.endGame(symbolToCheck);
+    }
+    if(tiles[0].contents===symbolToCheck &&tiles[3].contents===symbolToCheck &&tiles[6].contents===symbolToCheck ) // LEFT VERT
+    {
+        gameLoop.endGame(symbolToCheck);
+    }
+    if(tiles[1].contents===symbolToCheck &&tiles[4].contents===symbolToCheck &&tiles[7].contents===symbolToCheck ) // MID VERT
+    {
+        gameLoop.endGame(symbolToCheck);
+    }
+    if(tiles[2].contents===symbolToCheck &&tiles[5].contents===symbolToCheck &&tiles[8].contents===symbolToCheck ) // BOTTOM VERT
+    {
+        gameLoop.endGame(symbolToCheck);
+    }
+    if(tiles[0].contents===symbolToCheck &&tiles[4].contents===symbolToCheck &&tiles[8].contents===symbolToCheck ) // LEFT DIAG
+    {
+        gameLoop.endGame(symbolToCheck);
+    }
+    if(tiles[2].contents===symbolToCheck &&tiles[4].contents===symbolToCheck &&tiles[6].contents===symbolToCheck ) // RIGHT DIAG
+    {
+        gameLoop.endGame(symbolToCheck);
+    }
+  };
+    
 
-}());
+  return { tiles, render, gameBoardContainer,checkIfWin };
+})();
 
 // eslint-disable-next-line func-names, no-unused-vars
-const gameLoop = (function(){
-    const playerOne = Player('Player1','X',true);
+const gameLoop = (function () {
+  
+  let gameOver=false;
+  this.gameOver=gameOver;
+  function setGameOver()
+  {
+    gameOver=true;
+    return gameOver;
+  }
    
-    const playerTwo = Player("Player2", "O",false);
-    playerOne.setIsTurn();
-    playerTwo.setIsTurn();
-    console.log(playerOne);
-    console.log(playerTwo);
-    gameBoard.render();
-    const tileContainers = gameBoard.gameBoardContainer.querySelectorAll('.tile');
+  const playerOne = Player("Player1", "X", true);
 
-    tileContainers.forEach((item,index) =>{
-        item.addEventListener('click', () =>{
-            
-            
-            const thisTile = gameBoard.tiles[index];
-            
-           if(thisTile.getIsBlank())
-    {
-                    if(playerOne.getIsTurn())
-                    {
-                        thisTile.setTile('X');
-                        thisTile.setBlankFalse();
-                        console.log(thisTile.getIsBlank());
-                        
-                        
-                        item.textContent=thisTile.getTile();
-                        playerOne.switchTurn();
-                        playerTwo.switchTurn();
-                        
-                        
-                    }
-                else 
-                    {
-                        thisTile.setTile('O');
-                        thisTile.setBlankFalse();
-                        console.log(thisTile.getIsBlank());
-                        item.textContent = thisTile.getTile();
-                        playerOne.switchTurn();
-                        playerTwo.switchTurn();
-                    }
-                }
-           // }
-            // console.log(thisTile.getTile());
+  const playerTwo = Player("Player2", "O", false);
+  playerOne.setIsTurn();
+  playerTwo.setIsTurn();
+//   console.log(playerOne);
+//   console.log(playerTwo);
+  gameBoard.render();
+  const tileContainers = gameBoard.gameBoardContainer.querySelectorAll(".tile");
 
-        })
-    })
+  tileContainers.forEach((item, index) => {
+
+        const tileContainer = item;
+        tileContainer.addEventListener("click", () => {
+          const thisTile = gameBoard.tiles[index];
+            console.log(thisTile);
+          if (thisTile.getIsBlank()&&!gameOver) {
+            if (playerOne.getIsTurn()) {
+                thisTile.setTile("X");
+                thisTile.setBlankFalse();
+                
+                tileContainer.textContent = thisTile.getTile();
+                gameBoard.tiles[index].contents = thisTile.getTile();
+               
+                playerOne.switchTurn();
+                playerTwo.switchTurn();
+                gameBoard.checkIfWin('X');
+            } else if (playerTwo.getIsTurn()&&!gameOver){
+                thisTile.setTile("O");
+                thisTile.setBlankFalse();
+                tileContainer.textContent = thisTile.getTile();
+                gameBoard.tiles[index].contents = thisTile.getTile();
     
-}());
+                playerOne.switchTurn();
+                playerTwo.switchTurn();
+               
+                gameBoard.checkIfWin('O');
+    
+            }
+    
+        }
+       
+        
+        });
+      
+  });
+  function endGame(winningSymbol)
+  {
+      
+      
+      const game = gameBoard.gameBoardContainer;
+      console.log(gameOver);
+      const winPanel=document.createElement('div');
+      winPanel.className='win-panel';
+      winPanel.textContent= `${winningSymbol  } WINS!`;
+      
+      // game.remove();
+      document.body.appendChild(winPanel);
 
 
-
+  }
+return {endGame,setGameOver}})();
